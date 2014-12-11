0d1ae5bb29157f025694d8f62bf75de7e4d1c5da
Joshua Guy <josh@monsieur.co>
2014-12-11 12:55:36 -0500

    Don't worry about checking return values when toggling GPIOs

--- src/PosixSerialPort.cpp
+++ src/PosixSerialPort.cpp
@@ -102,18 +102,16 @@ PosixSerialPort::~PosixSerialPort()
 bool
 PosixSerialPort::initcmd()
 {
-    int ret;
-    
     system("echo 117 > /sys/class/gpio/export");
     system("echo 0 > /sys/class/gpio/export"); 
-    ret = system("echo \"out\" > /sys/class/gpio/gpio117/direction");
-    ret = system("echo 1 > /sys/class/gpio/gpio117/value");
+    system("echo \"out\" > /sys/class/gpio/gpio117/direction");
+    system("echo 1 > /sys/class/gpio/gpio117/value");
     usleep(300000);
-    ret = system("echo \"in\" > /sys/class/gpio/gpio117/direction");
-    ret = system("echo \"out\" > /sys/class/gpio/gpio0/direction");
-    ret = system("echo 0 > /sys/class/gpio/gpio0/value");
+    system("echo \"in\" > /sys/class/gpio/gpio117/direction");
+    system("echo \"out\" > /sys/class/gpio/gpio0/direction");
+    system("echo 0 > /sys/class/gpio/gpio0/value");
     usleep(100000);
-    ret = system("echo 1 > /sys/class/gpio/gpio0/value");
+    system("echo 1 > /sys/class/gpio/gpio0/value");
 
     return true;
 }
